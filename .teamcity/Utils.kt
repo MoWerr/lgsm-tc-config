379a87f21fa726410c9d05895329f52c2ddf038e
MoWer <mbaran2601@gmail.com>
2021-05-15 18:57:17 +0200

    Compilation test

--- .teamcity/Utils.kt
+++ .teamcity/Utils.kt
@@ -1,4 +1,4 @@
 import jetbrains.buildServer.configs.kotlin.v10.toExtId
 
 fun generateId(type: String, repoName: String, branchName: String) = "${type}${repoName}${branchName}".toExtId()
-fun generateId(type: String, vcsRoot: Settings.LgsmRoot): String = generateId(type, vcsRoot.repoName, vcsRoot.branchName)
+fun generateId(type: String, vcsRoot: LgsmRoot): String = generateId(type, vcsRoot.repoName, vcsRoot.branchName)
