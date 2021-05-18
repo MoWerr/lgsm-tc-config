be0beb1c0055aed31667d8b0bcf312082d182eb3
MoWer <mbaran2601@gmail.com>
2021-05-18 21:12:34 +0200

    Dependency id will be stored in the context param

--- .teamcity/Projects.kt
+++ .teamcity/Projects.kt
@@ -1,4 +1,5 @@
 import jetbrains.buildServer.configs.kotlin.v2019_2.AbsoluteId
+import jetbrains.buildServer.configs.kotlin.v2019_2.DslContext
 import jetbrains.buildServer.configs.kotlin.v2019_2.Project
 
 open class MainSubproject(vscRoot: LgsmRoots, baseProject: AbsoluteId? = null) : Project({
@@ -18,7 +19,7 @@ open class ProjDev(vcsRoot: LgsmRoot, baseProject: AbsoluteId? = null) : Project
     buildType(PromoteToStable(vcsRoot, build))
 })
 
-object ProjBase : MainSubproject(VcsBase, AbsoluteId("UbuntuBase_Stable_Build"))
+object ProjBase : MainSubproject(VcsBase, AbsoluteId(DslContext.getParameter("dependencyId")))
 object ProjVHServer : MainSubproject(VcsVHServer, generateAbsoluteId(IdType.Build, VcsBase.master))
 object ProjArkServer : MainSubproject(VcsARKServer, generateAbsoluteId(IdType.Build, VcsBase.master))
 
